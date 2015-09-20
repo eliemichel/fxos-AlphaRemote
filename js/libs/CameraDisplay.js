be209717001cf87828f3007872bc7d7fa48aa567
Élie Michel <elie.michel@ens.fr>
2015-09-20 14:40:16 -0400

    Replace Transport by Stream
    A Promise-driver stream reader.

--- js/libs/CameraDisplay.js
+++ js/libs/CameraDisplay.js
@@ -8,7 +8,6 @@ function CameraDisplay(camera, canvas) {
   this.camera = camera;
   this.canvas = canvas;
   
-  this.transport = new Transport();
   this.context = this.canvas.getContext("2d");
   this.imageData = this.context.createImageData(this.canvas.width, this.canvas.height);
   
@@ -27,152 +26,93 @@ CameraDisplay.prototype.startLiveviewStreaming = function() {
   .then(res => {
     this.liveviewEndpoint = res[0];
 
-    var socket = navigator.mozTCPSocket.open("192.168.122.1", 8080, 'arraybuffer');
+    var socket = navigator.mozTCPSocket.open("192.168.122.1", 8080, {binaryType: 'string'});
+    console.log(socket.binaryType);
     socket.onopen = () => socket.send("GET /liveview/liveviewstream HTTP/1.1\r\nHost: 192.168.122.1\r\n\r\n");
-    this.transport.listen(socket);
-
-    this.streamingState = 1;
-    
-    var poll = () => {
-      try {
-        this.updateLiveviewStreaming();
-        if (this.streamingState < 4) {
-          // Try to continue running until WaitException is thrown
-          setTimeout(() => poll(), CameraDisplay.UPDATE_PERIOD);
-        }
-      } catch(ex) {
-        if (ex instanceof Transport.WaitException) {
-          setTimeout(() => poll(), CameraDisplay.UPDATE_PERIOD);
-        } else {
-          throw ex;
-        }
-      }
-    };
-    //poll();
-    
+    var stream = new Stream(socket);
+    stream.read(4)
+    .then(data => {
+      console.log(data);
+    })
   });
 };
-
-// This function can raise WaitException and so must remember its state
-CameraDisplay.prototype.updateLiveviewStreaming = function() {
-  console.log("updateLiveviewStreaming", this.streamingState);
-  if (this.streamingState == 0) {
-    // streaming not started
-    return;
-  } else if (this.streamingState == 1) {
-    // init HTTP header
-    this.httpHeader = new Array();
-    this.streamingState = 2;
-  } else if (this.streamingState == 2) {
-    // consolidating HTTP header
-    var line = this.transport.readline();
-    this.httpHeader.push(line);
-
-    if (line == '') {
-      // End Of HTTP Header
-      console.log("Header:", this.httpHeader);
-      this.streamingState = 2.5;
-    }
-  } else if (this.streamingState == 2.5) {
-    var line = this.transport.readline();
-    this.streamingState = 3;
-  } else if (this.streamingState == 3) {
-    var payloadType, frameId, timestamp;
-    this.transport.transaction(() => {
-      var magick = this.transport.read(1);
-      if (magick != 0xff) {
-        console.log("Warning: Wrong magick: " + magick);
-      }
-      payloadType = this.transport.read(1);
-      console.log("liveview payload? " + (payloadType == 0x01));
-      frameId = (new Uint16Array(this.transport.read(2)))[0];
-      console.log("frameId: " + frameId);
-      timestamp = (new Uint32Array(this.transport.read(4)))[0];
-      console.log("timestamp: " + timestamp);
-      this.streamingState = 4;
-    });
-
-  //this.context.putImageData(this.imageData, 0, 0); 
-  }
-};
-
 
 
 // If only XmlHttpRequest allowed to perform streaming downloading, I would not have to
 // recode this basic HTTP handler...
 
-function Transport() {
-  this.socket = undefined;
-  this.fullBuffer = new Uint8Array();
-  this.transactionRunning = false;
-  this.transactionBuffer = new Uint8Array();
+function Stream(socket) {
+  this._socket = undefined;
+  this._fullBuffer = new ArrayBuffer(Stream.MAX_BUFFER_SIZE * 2);
+  this._bufferViewStart = 0;
+  this._bufferViewEnd = 0; // actually, next empty cell
+  this._transactionLock = false;
+  this._transactionResolve = () => {};
+  this._waitedByteLength = 0;
+  
+  if (socket) {
+    this.listen(socket);
+  }
 }
 
-Transport.READLINE_CHUNK_SIZE = 128;
+Stream.MAX_BUFFER_SIZE = 1024;
 
-Transport.WaitException = function() {
-  this.name =        "Wait Exception";
-  this.message =     "Need to wait for data. Please try again later.";
-  this.toString =    () => (this.name + ": " + this.message);
+Stream.prototype.listen = function(socket) {
+  this._socket = socket;
+  this._socket.ondata = this._onData.bind(this);
 };
 
-Transport.prototype.listen = function(socket) {
-  this.socket = socket;
-  socket.ondata = ev => this.fullBuffer += ev.data;
-};
-
-Transport.prototype.read = function(n) {
-  if (this.fullBuffer.length < n) {
-    throw new Transport.WaitException();
+Stream.prototype._onData = function(evt) {
+  // This is a dirty hack
+  var buf = new ArrayBuffer(evt.data.length*2);
+  var bufView = new Uint16Array(buf);
+  for (var i=0, strLen=evt.data.length; i<strLen; i++) {
+    bufView[i] = evt.data.charCodeAt(i);
   }
-
-  var out = this.fullBuffer.substring(0, n);
-  this.fullBuffer = this.fullBuffer.substring(n);
-  if (this.transactionRunning) {
-    this.transactionBuffer += out;
-  }
-  return out;
-};
-
-Transport.prototype.readline = function() {
-  var buf = '';
-  this.transaction(() => {
-    while (buf.indexOf('\n') == -1) {
-      buf += this.read(Transport.READLINE_CHUNK_SIZE);
+  
+  // Append data to buffer
+  (new Uint8Array(buf)).copyWithin(this._fullBuffer, this._bufferViewEnd);
+  this._bufferViewEnd += evt.data.length;
+  
+  var bufferLength = this._bufferViewEnd - this._bufferViewStart;
+  if (this._waitedByteLength > 0) {
+    if (bufferLength >= this._waitedByteLength) {
+      var waitedLength = this._waitedByteLength; // * 1.0 because we use Uint8Array
+      var view = new Uint8Array(this._fullBuffer, this._bufferViewStart, waitedLength);
+      
+      this._bufferViewStart += this._waitedByteLength;
+      this._waitedByteLength = 0;
+      
+      this._transactionResolve(view.slice()); // slice is used to copy buffer data
     }
-    var n = buf.indexOf('\n');
-    this.fullBuffer = buf.substring(n + 1) + this.fullBuffer;
-    buf = buf.substring(0, n);
-    if (buf.length > 0 && buf.charAt(buf.length - 1) == '\r') {
-      buf = buf.substring(0, buf.length - 1);
+  } else if (bufferLength > Stream.MAX_BUFFER_SIZE) {
+    console.log("Warning: MAX_BUFFER_SIZE (" + Stream.MAX_BUFFER_SIZE + ") excedeed. Discarding all buffer.");
+    this._bufferViewStart = 0;
+    this._bufferViewEnd = 0;
+  }
+  
+  // Cycling buffer data
+  if (this._bufferViewStart > this._fullBuffer.length - Stream.MAX_BUFFER_SIZE) {
+    bufferLength = this._bufferViewEnd - this._bufferViewStart;
+    (new Uint8Array(this._fullBuffer, this._bufferViewStart, bufferLength)).copyWithin(this._fullBuffer);
+    this._bufferViewEnd -= this._bufferViewStart;
+    this._bufferViewStart = 0;
+  }
+};
+
+Stream.prototype.read = function(len) {
+  if (this._transactionLock) {
+    console.log("Transation lock already active.");
+    return;
+  }
+  this._waitedByteLength = len;
+  this._transactionLock = true;
+  
+  return new Promise((resolve, reject) => {
+    this._transactionResolve = data => {
+      this._transactionLock = true;
+      resolve(data);
     }
   });
-  return buf;
 };
 
-// Restores previous buffer state if something goes wrong (e.g. WaitException occurs)
-Transport.prototype.transaction = function(transaction) {
-  var prevTransactionBuffer = this.transactionBuffer;
-  var prevTransactionRunning = this.transactionRunning;
-  this.transactionBuffer = new Uint8Array();
-  this.transactionRunning = true;
-  
-  var exception = undefined;
-  
-  try {
-    transaction();
-  } catch(ex) {
-    if (ex instanceof Transport.WaitException) {
-      console.log("Fail with buffer of size: " + this.transactionBuffer.length);
-      console.log("While full buffer size is: " + this.fullBuffer.length);
-      // Refill buffer
-      this.fullBuffer = this.transactionBuffer + this.fullBuffer;
-    }
-    exception = ex;
-  }
-  
-  this.transactionRunning = prevTransactionRunning;
-  this.transactionBuffer = prevTransactionBuffer;
-  
-  if (exception) throw exception;
-}
